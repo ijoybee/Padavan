e7840b5145c17c312baf5c75b132d6a3402e240e
ijoybee <60688732+ijoybee@users.noreply.github.com>
2026-05-09 09:31:07 +0800

    Update update_gfwlist.sh

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -3,10 +3,13 @@
 set -e -o pipefail
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 #GFWLIST_URL="$(nvram get ss_gfwlist_url)"
+logger -st "gfwlist" "Starting update..."
+# 1. 从 NVRAM 中读取前端网页保存的自定义地址
+CUSTOM_GFWLIST_URL="$(nvram get ss_gfwlist_url)"
 # 2. 判断用户是否填写了自定义地址
-if [ -n "$ss_gfwlist_url" ]; then
+if [ -n "$CUSTOM_GFWLIST_URL" ]; then
     # 如果填写了，就用自定义的
-    GFWLIST_URL="$ss_gfwlist_url"
+    GFWLIST_URL="$CUSTOM_GFWLIST_URL"
     logger -st "gfwlist" "使用自定义 gfwlist 地址..."
 else
     # 如果没填（为空），则回退使用默认的备用地址
@@ -15,7 +18,6 @@ else
 fi
 # 3. 执行 curl 下载，将原本写死的地址替换为变量 "$GFWLIST_URL"
 curl -k -s -o /tmp/gfwlist_list_origin.conf --connect-timeout 15 --retry 5 "$GFWLIST_URL"
-
 lua /etc_ro/ss/gfwupdate.lua
 count=`awk '{print NR}' /tmp/gfwlist_list.conf|tail -n1`
 if [ $count -gt 1000 ]; then
